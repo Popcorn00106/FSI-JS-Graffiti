4be1ac9506cf07e0c17dcd2c5f97b1edf747aa0a
Dylan LaRue <dylan@Dylans-iMac.askey.com>
2023-01-04 19:54:51 -0800

    added style in .js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Select some elements...
+let pageHeader = document.querySelectorAll("#page-header")
+
+console.log(pageHeader)
+for(let i = 0; i < pageHeader.length; i++) {
+    pageHeader[i].style.color = "green"
+    pageHeader[i].style.fontFamily = "Serif-sans"
+}
+
+
+
+let dogNames = document.querySelectorAll(".dog-name")
+console.log(dogNames)
+for(let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.color = "red"
+}
+ let dogImages = document.querySelectorAll("img")
+ 
